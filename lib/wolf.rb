d835babd77d8981cb82f007baa49c65bfb2d9942
Rodney Cullen <rodcul@gmail.com>
2015-04-28 23:08:15 +0100

    Refactory based on @andygout conditional
    https://github.com/andygout/misc_challenges/blob/master/stocks%20(using%20array%20as%20argument)

--- lib/wolf.rb
+++ lib/wolf.rb
@@ -1,17 +1,13 @@
 def wolf(stockprice)
-  output = {}
-  buy_sell_days = []
+  output = { buy_day: nil, sell_day: nil }
   profit = 0
   stockprice.each_with_index do |item1, index1|
     stockprice.each_with_index do |item2, index2|
-      next if index2 < index1
-      diff = item2 - item1
-      if diff > profit
-        profit = diff
-        buy_sell_days = index1, index2
+      if index2 > index1 && (item2 - item1) > profit
+        profit = item2 - item1
+        output = { buy_day: index1, sell_day: index2 }
       end
     end
-    output = { buy_day: buy_sell_days[0], sell_day: buy_sell_days[1] }
   end
   output
 end
