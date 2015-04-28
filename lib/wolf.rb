10584ebeb9336e528aa78bf4100d1c4dfc603f3e
Rodney Cullen <rodcul@gmail.com>
2015-04-28 22:38:35 +0100

    Refactor first attempt (rubocop)

--- lib/wolf.rb
+++ lib/wolf.rb
@@ -1,23 +1,17 @@
 def wolf(stockprice)
   output = {}
-  array = []
-  diff = 0
-  stockprice.each_with_index do |item1,index1|
-    stockprice.each_with_index do |item2,index2|
-
-      n = 0
-      if index2 > index1
-        n = item2-item1
-      if n > diff
-        diff = n
-        array = index1,index2
+  buy_sell_days = []
+  profit = 0
+  stockprice.each_with_index do |item1, index1|
+    stockprice.each_with_index do |item2, index2|
+      next if index2 < index1
+      diff = item2 - item1
+      if diff > profit
+        profit = diff
+        buy_sell_days = index1, index2
       end
     end
-
-
-    end
-    output[:buy_day] = array[0]
-    output[:sell_day] = array[1]
+    output = { buy_day: buy_sell_days[0], sell_day: buy_sell_days[1] }
   end
   output
 end
--- spec/wolf_spec.rb
+++ spec/wolf_spec.rb
@@ -1,24 +1,19 @@
 require 'wolf'
 
 describe 'wolf' do
-
-
   it 'Base case: For [10,2,5,8,9,1,6,7,8,10] buy on 5, sell on 9' do
-    stockprice = [10,2,5,8,9,1,6,7,8,10]
+    stockprice = [10, 2, 5, 8, 9, 1, 6, 7, 8, 10]
     days = { buy_day: 5, sell_day: 9 }
     expect(wolf(stockprice)).to eq days
   end
-
   it 'Where best days are not highest days: For [7, 9, 7, 5, 8] buy on 3, sell on 4' do
     stockprice = [7, 9, 7, 5, 8]
     days = { buy_day: 3, sell_day: 4 }
     expect(wolf(stockprice)).to eq days
   end
-
   it 'Error handling: For [10,9,8,7,6,5,4,3,2,1] do not buy' do
-    stockprice = [10,9,8,7,6,5,4,3,2,1]
+    stockprice = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     days = { buy_day: nil, sell_day: nil }
     expect(wolf(stockprice)).to eq days
   end
-
 end
